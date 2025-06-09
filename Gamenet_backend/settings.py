271f9fd1e952b408c703fc6dda9c17a7890d9b2a
Armin Emami <armemm2001@gmail.com>
2025-06-09 15:45:37 +0330

    Connecting front

--- Gamenet_backend/settings.py
+++ Gamenet_backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usersapi',
-    
+    'corsheaders',
     'ninja_extra',
   
 ]
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
